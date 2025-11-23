199e25ac6a8fe00485481bd85a836b913a5a38c4
sypnoze <onurkucukkaya.webdev@gmail.com>
2025-11-23 13:34:24 +0100

    git & gitignore fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import AppHeader from "@/components/AppHeader.vue";
+import TodoList from "@/components/TodoList.vue";
+import AppFooter from "@/components/AppFooter.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AppHeader />
+  <main>
+    <TodoList />
+  </main>
+  <AppFooter />
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
