a9a84a89b167b1f3e7fb357276ca475655e5b7f2
sypnoze <onurkucukkaya.webdev@gmail.com>
2025-11-24 13:01:42 +0100

    Envoi de l'ApiURL à la TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup>
 import AppHeader from "@/components/AppHeader.vue";
-import TodoList from "@/components/TodoList.vue";
+import TodoList from "@/components/todoList/TodoList.vue";
 import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
   <AppHeader />
-  <main>
-    <TodoList />
+  <main class="w-full max-w-xl mt-8 bg-slate-200 rounded-xl">
+    <TodoList apiURL="https://6923036209df4a4923240031.mockapi.io/" />
   </main>
   <AppFooter />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,8 @@
 <script setup></script>
-<template></template>
+<template>
+  <!-- FOOTER GLOBAL -->
+  <footer class="my-6 text-slate-400 text-sm text-center">
+    <p>Simple Todo Template — Tailwind CSS · EAFC 2025</p>
+  </footer>
+</template>
 <style scoped></style>
